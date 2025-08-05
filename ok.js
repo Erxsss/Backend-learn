202dbfa773f675ea2bdacdd4b7074f31de271eb5
25HP4283 25HP4283 <25HP4283@G301-CC0211.localdomain>
2025-08-05 17:27:17 +0800

    hello

--- ok.js
+++
@@ -1,7 +0,0 @@
-
-function comb(list1, list2) {
-  const combine = list1.concat(list2);
-  combine.sort((a,b)=> a-b);
-  return combine;
-}
-console.log(comb((list1 = [1, 2, 4]), (list2 = [1, 3, 4])));

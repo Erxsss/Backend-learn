dcbf39b0d7fe3efc4711c77e2f10a1350ed87f16
25HP4283 25HP4283 <25HP4283@G301-CC0211.localdomain>
2025-08-05 17:24:15 +0800

    hi

--- my-app/src/app/_components/com.jsx
+++ my-app/src/app/_components/com.jsx
@@ -17,7 +17,32 @@ const Mycom = () => {
   useEffect(() => {
     fetchdata();
   }, []);
-  return <div>{countries[0]?.fifa} </div>;
+  return (
+    <div className="m-[30px]">
+      <div className="flex flex-col gap-[100px] text-[50px]">
+        <div>FLAGS OF THE WORLD</div>
+        <div className="flex justify-between">
+          <div>{countries.length} countries in the world</div>
+          <div>
+            <input placeholder="Enter country" className="border-[3px] rounded-xl border-gray-500"/>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-10 ml-30 mr-30 mt-[200px]">
+        <div className="flex flex-wrap gap-20 m-20 justify-center">
+          {countries.map((country, index) => {
+            const name = country.name.common;
+            return (
+              <div key={index} className="flex flex-col gap-2">
+                <img src={country.flags.png} alt="" />
+                <div className="text-[30px] flex justify-center">{name}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mycom;
--- my-app/src/app/hello/page.jsx
+++ my-app/src/app/hello/page.jsx
@@ -3,7 +3,6 @@ import Mycom from "../_components/com.jsx";
 export default function Home() {
   return (
     <div>
-      <div>Data:</div>
       <Mycom />
     </div>
   );

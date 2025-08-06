709a8eb6fc5adb626cf920bedb3901db1d0bb857
25HP4283 25HP4283 <25HP4283@G301-CC0211.localdomain>
2025-08-06 16:53:40 +0800

    aasdf

--- my-app/src/app/_components/com.jsx
+++ my-app/src/app/_components/com.jsx
@@ -25,36 +25,37 @@ const Mycom = () => {
   useEffect(() => {
     fetchdata();
   }, []);
-  if (many > 0) {
-    return (
-      <div className="c">
-        <div className="flex flex-col gap-[100px] text-[50px]">
-          <div className="color-[white]">FLAGS OF THE WORLD</div>
-          <div className="flex justify-between">
-            <div>{many} countries in the world</div>
-            <div>
-              <input
-                placeholder="Enter country"
-                className="border-[3px] rounded-xl border-gray-500"
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-            </div>
+
+  const filteredCountries = countries.filter((a) => {
+    return a.name.common.toLowerCase().includes(inputValue.toLowerCase());
+  });
+
+  return (
+    <div className="c">
+      <div className="flex flex-col gap-[100px] text-[50px]">
+        <div className="color-[white]">FLAGS OF THE WORLD</div>
+        <div className="flex justify-between">
+          <div>{many} countries in the world</div>
+          <div>
+            <input
+              placeholder="Enter country"
+              className="border-[3px] rounded-xl border-gray-500"
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
           </div>
         </div>
-        <div className="flex flex-col gap-10 ml-30 mr-30 mt-[200px] ">
-          <div className="flex flex-wrap gap-20 m-20 justify-center ">
-            {countries
-              .filter((a) => {
-                return a.name.common.toLowerCase().includes(inputValue);
-              })
-              .map((country, index) => {
+      </div>
+      <div className="flex flex-col gap-10 ml-30 mr-30 mt-[200px] ">
+        <div className="flex flex-wrap gap-20 m-20 justify-center text-7xl">
+          {filteredCountries.length > 0
+            ? filteredCountries.map((country, index) => {
                 const name = country.name.common;
                 return (
                   <div
                     key={index}
-                    className="flex flex-col gap-2 border-[3px] p-7 rounded-xl border-gray-500 bg-[white] "
+                    className="flex flex-col gap-2 border-[3px] p-7 rounded-xl border-gray-500 bg-white hover:bg-blue-700 hover:text-amber-50 transition-colors duration-300 ease-in-out"
                   >
                     <img src={country.flags.png} alt="" />
                     <div className="text-[30px] flex justify-center">
@@ -62,33 +63,12 @@ const Mycom = () => {
                     </div>
                   </div>
                 );
-              })}
-          </div>
+              })
+            : "NOT FOUND"}
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="c">
-        <div className="flex flex-col gap-[100px] text-[50px] justify-center">
-          <div className="color-[white]">FLAGS OF THE WORLD</div>
-          <div className="flex justify-between">
-            <div>{many} countries in the world</div>
-            <div>
-              <input
-                placeholder="Enter country"
-                className="border-[3px] rounded-xl border-gray-500"
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="flex justify-center">NO COUNTRY BRO</div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Mycom;
